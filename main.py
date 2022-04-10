fddd6ca8f010e8d3a9a6b95247989cb4e043cb11
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-10 02:38:02 +0100

    changed lang

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands, tasks
+
+load_dotenv()
+
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    print("Bot logged in")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Hello")
+
+bot.run(os.environ.get("TOKEN"), bot = False)
+
