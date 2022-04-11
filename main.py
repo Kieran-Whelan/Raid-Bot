b62eddfd8d9451f8f7fac625dc201ad367937fa6
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-11 01:27:17 +0100

    fix

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from discord.ext import commands, tasks
 
 load_dotenv()
 
-intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
-bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+bot = commands.Bot(command_prefix = "!", intents = intents, help_command = None)
 
 @bot.event
 async def on_ready():
@@ -15,6 +15,11 @@ async def on_ready():
 @bot.command()
 async def test(ctx):
     await ctx.send("Hello")
+
+@bot.command()
+async def spamping(ctx, member, repeat):
+    for x in range(0, int(repeat)):
+        await ctx.send(f"{member}")
 
 token = os.getenv("TOKEN")
 bot.run(token, bot = False)
