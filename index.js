30a8fc1fec95199b086acbf91aba78df0f87cbd3
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-15 01:46:01 +0100

    added more

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const chalk = new require('chalk');
 const dotenv = require('dotenv');
+const prefix = "!";
 
 dotenv.config()
 
-client.on('ready', msg => {
-    /*
+client.on('ready', () => {
     console.log("");                                   
-    console.log((chalk.cyan(`                                            #####                           `)));
-    console.log((chalk.cyan(`                                           #        #####          `)));
-    console.log((chalk.cyan(`                                           #        #    #        `)));
-    console.log((chalk.cyan(`                                           #####    #    #        `)));
-    console.log((chalk.cyan(`                                           #        #####      `)));
-    console.log((chalk.cyan(`                                           #        #    #       `)));
-    console.log((chalk.cyan(`                                           #        #      #      `)));
+    console.log((chalk.green(`#####    #####      ####`)));
+    console.log((chalk.green(`#        #    #    #    #`)));
+    console.log((chalk.green(`#        #    #    #    #`)));
+    console.log((chalk.green(`#####    #####     #    #`)));
+    console.log((chalk.green(`#        #   #     #    #`)));
+    console.log((chalk.green(`#        #    #    #    #`)));
+    console.log((chalk.green(`#        #     #    ####`)));
     console.log("");                                                                  
-    console.log((chalk.red(`                                                         Discord: FrogDog#4630`)));   
-    console.log((chalk.red(`                                                        Github: https://github.com/GalackQSM`)));  
-    console.log("");     
-    */
-   console.log("Logged in as")                             
+    console.log((chalk.cyan(`Discord: FrogDog#4630`)));   
+    console.log((chalk.cyan(`Github: https://github.com/GalackQSM`)));  
+    console.log(""); 
+    console.log((chalk.red(`Successfully logged in as ${client.user.tag}`)));                             
 });
 
-client.login(process.env.TOKEN)
+client.on('message', (msg) => {
+    if (msg.content.startsWith(prefix)) {
+        if (msg.content.includes("spam")) {
+            msg.channel.send("spam")
+        }
+    }
+});
+
+client.login(process.env.TOKEN);
