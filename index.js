329f5a070bf68e7568ded473d0bd66a6bfb6fd71
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-08-19 18:41:49 +0100

    added copypasta webscraper

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const chalk = new require('chalk');
 const dotenv = require('dotenv');
+const request = require('request');
+const {readFileSync, promises: fsPromises} = require('fs');
 const prefix = "$";
 
 dotenv.config();
 
+let htmlText = "";
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomUrl() {
+    const contents = readFileSync("copypasta.txt", 'utf-8');
+    const arr = contents.split(/\r?\n/);
+
+    return arr[getRandomInt(0, arr.length - 1)];
+}
+
+function fetch() {
+    request("https://copypastatext.com/" + getRandomUrl() + "/", function (error, response, body) {
+        htmlText = body.match(/<code>([^<]+)<\/code>/)[1].toString();
+
+        if (htmlText.length > 1900) {
+            htmlText = htmlText.substring(0, 1900);
+        }
+    }) 
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,17 +43,30 @@ client.on('ready', async () => {
     console.log((chalk.green(`#        #    #    #    #   #        #    #        # #            #    #   `)));
     console.log((chalk.green(`#        #    #    #    #   #        #    #       #   #       #   #     #  `)));
     console.log((chalk.green(`#####    #####     #    #   #  ##    #####        #####       #   #      # `)));
-    console.log((chalk.green(`#        #   #     #    #   #    #   #   #       #      #     #   #     #  `)));
-    console.log((chalk.green(`#        #    #    #    #   #    #   #    #     #        #    #   #    #   `)));
-    console.log((chalk.green(`#        #     #    ####     ####    #     #   #          #   #   #####    `)));
+    console.log((chalk.green(`#        #   #     #    #   #    #   #   #       #     #      #   #     #  `)));
+    console.log((chalk.green(`#        #    #    #    #   #    #   #    #     #       #     #   #    #   `)));
+    console.log((chalk.green(`#        #     #    ####     ####    #     #   #         #    #   #####    `)));
     console.log("");                                                                  
     console.log((chalk.cyan(`Discord: FrogDog#4630`)));   
-    console.log((chalk.cyan(`Github: https://github.com/FrogDog56`)));  
+    console.log((chalk.cyan(`Github: https://github.com/FrogDog56`)));
+    console.log((chalk.cyan(`Repo: https://github.com/FrogDog56/Raid-Bot`)));    
     console.log(""); 
-    console.log((chalk.red(`Successfully logged in as ${client.user.tag}`)));                             
+    console.log((chalk.red(`Successfully logged in as ${client.user.tag}`)));
+
+    client.user.setActivity({
+        game : {
+            name : "Heck",
+            type : "PLAYING"
+        }
+    });
 });
 
 client.on('message', async (msg) => {
+    //user check
+    if (msg.author.id != "652627153418190881") {
+        return;
+    }
+
     //checks for client prefix
     if (msg.content.toString()[0] === prefix) {
         let msgStr = msg.content.toString().concat(" ", "end");
@@ -52,6 +91,10 @@ client.on('message', async (msg) => {
                 msg.channel.send(msgWords.toString().replaceAll(",", " "));
                 await sleep(500);
             }
+        } else if (args[0] === "shitpost") {
+            fetch();
+            await sleep(1000);
+            msg.channel.send(htmlText);
         } else {
             msg.channel.send("Error: no such command exists!");
         }
